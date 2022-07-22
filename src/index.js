a445204d23cc4dd1255f08e705054fe5e8142adc
amishra6391 <95343054+amishra6391@users.noreply.github.com>
2022-07-22 20:32:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './fechapiaync/App';
+ import App from './App';
 // import Isrodataapi from './isrodatafechapi/Isrodataapi'
-import Todolistcode from './todoloist/Todolistcode';
-import reportWebVitals from './reportWebVitals';
+//import Todolistcode from './todoloist/Todolistcode';
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App /> 
     {/* <Isrodataapi/> */}
     <Todolistcode />
   </React.StrictMode>
